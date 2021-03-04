e999317a14dd85f3336048fda339089fbd82cc81
Bralediro <brayan0712@gmail.com>
2021-03-04 00:14:59 -0500

    ref_con

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@
 //console.log("Hola mundo")
 
 import {client} from './config/conn.ts';
+import { UsuariosModel } from "./models/user.ts";
+import { UsuarioDTO } from "./dto/usuario.ts";
+
+const usuariosModel = new UsuariosModel();
+
+
 let crud
 
 do {
@@ -19,23 +25,23 @@ do {
         case 1: {
             console.log("Registro de usuarios")
 
-            let nombre = prompt("Ingrese su nombre: ") as string;
-            let apellido = prompt("Ingrese su apellido: ") as string;
-            let celular = prompt("Ingrese su numero de telefono: ") as string;
-            let correo = prompt("Ingrese su correo: ") as string;
-            let password = prompt("Digite su contraseña: ") as string;
+            const nombre = prompt("Ingrese su nombre: ") as string;
+            const apellido = prompt("Ingrese su apellido: ") as string;
+            const celular = prompt("Ingrese su numero de telefono: ") as string;
+            const correo = prompt("Ingrese su correo: ") as string;
+            const password = prompt("Digite su contraseña: ") as string;
 
             if (nombre.length > 3 && apellido.length > 3 && celular.length >= 10 && password.length >= 8) {
+                await usuariosModel.crear({
+                    nombre: nombre,
+                    apellido: apellido,
+                    celular: celular,
+                    correo: correo,
+                    password: password,
+                });
 
-                const usuarios = await client.execute('INSERT INTO usuarios (nombre,apellido,celular,correo,password) values (?,?,?,?,?)', [
-                    nombre,
-                    apellido,
-                    celular,
-                    correo,
-                    password,
-                ]);
                 console.log("Registro guardado")
-                console.log(usuarios.rows)
+                //console.log()
             } else {
                 console.log("Por favor llenar el formulario correctamente")
             }
@@ -45,7 +51,7 @@ do {
 
         case 2 : {
 
-            const usuarios = await client.query('select * from usuarios');
+            const  usuarios = await usuariosModel.listar();
             console.log(usuarios);
             let id_usuario = prompt("Seleccione el ID que desea actualizar") as string;
             let nombre = prompt("Ingrese su nombre") as string;
@@ -67,16 +73,16 @@ do {
         }
 
         case 3 : {
-
-            const usuarios = await client.execute('select * from usuarios');
-            console.log(usuarios.rows);
+            const  usuarios = await usuariosModel.listar();
+            // usuarios = await client.execute('select * from usuarios');
+            console.log(usuarios);
             break;
 
         }
 
         case 4 : {
 
-            const usuarios = await client.query(`select * from usuarios`);
+            const  usuarios = await usuariosModel.listar();
             console.log(usuarios);
             let id = prompt("Seleccione el ID que desea eliminar") as string;
             let select = await client.execute(`delete from usuarios where id = ?`, [id])
